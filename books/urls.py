2b8401d49d2dffc128db5b48b43662cd0ffbaed9
l-gorka <gorka.lukasz88@gmail.com>
2021-12-12 23:10:17 +0100

    - added list view with search and pagination

--- books/urls.py
+++ books/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 from django.views.generic.base import TemplateView
+
+import library
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('base/', TemplateView.as_view(template_name='base-menu.html'), name='base'),
+    path('', include('library.urls', namespace='library')),
 ]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,33 @@
+from django.core import paginator
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Q
+from library.models import Book
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-# Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book-list.html'
+    
+    def get(self, request):
+        page = request.GET.get('page', 1)
+        search = request.GET.get('search', False)
+
+        if search:
+            query = Q(title__icontains=search)
+            query.add(Q(description__icontains=search), Q.OR)
+            book_list = Book.objects.filter(query).select_related()
+        else:
+            book_list = Book.objects.all()
+            search = ''
+        paginator = Paginator(book_list, 100)
+        try:
+            books = paginator.page(page)
+        except PageNotAnInteger:
+            books = paginator.page(1)
+        except EmptyPage:
+            books = paginator.page(paginator.num_pages)
+        context = {'book_list': books, 'search': search}
+        return render(request, self.template_name, context)
+
