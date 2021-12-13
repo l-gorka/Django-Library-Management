b1ac7bbc7fdb27254d8307aaa194a0d1122811e6
l-gorka <gorka.lukasz88@gmail.com>
2021-12-13 21:58:17 +0100

    fixed search by author and genre

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'library'
 urlpatterns = [
-    path('list/', views.BookListView.as_view(), name='book-list')
+    path('list/', views.BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,5 @@
-from django.core import paginator
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.db.models import Q
 from library.models import Book
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
@@ -9,14 +8,15 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 class BookListView(ListView):
     model = Book
     template_name = 'book-list.html'
-    
+
     def get(self, request):
         page = request.GET.get('page', 1)
         search = request.GET.get('search', False)
 
         if search:
             query = Q(title__icontains=search)
-            query.add(Q(description__icontains=search), Q.OR)
+            query.add(Q(authors__name__icontains=search), Q.OR)
+            query.add(Q(genre__genre_name__icontains=search), Q.OR)
             book_list = Book.objects.filter(query).select_related()
         else:
             book_list = Book.objects.all()
@@ -31,3 +31,7 @@ class BookListView(ListView):
         context = {'book_list': books, 'search': search}
         return render(request, self.template_name, context)
 
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'detail.html'
