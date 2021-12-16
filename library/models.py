91d69e62cc5e53372c5ebae694917c800aaffe3f
l-gorka <gorka.lukasz88@gmail.com>
2021-12-16 20:32:24 +0100

    fix: users must have unique emails

--- library/models.py
+++ library/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -25,3 +26,6 @@ class Genre(models.Model):
 
     def __str__(self) -> str:
         return self.genre_name
+
+
+User._meta.get_field('email')._unique = True
