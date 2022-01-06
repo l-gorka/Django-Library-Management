7f2e3588d4033a000091d3a2064b0a859ed6ec5d
l-gorka <gorka.lukasz88@gmail.com>
2022-01-06 10:53:49 +0100

    Detail view changed.

--- library/models.py
+++ library/models.py
@@ -1,7 +1,5 @@
-from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import SET_NULL
 from django.db.models.signals import post_save
 from datetime import datetime
 
